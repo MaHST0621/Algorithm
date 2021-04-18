5d0d1bb68aa3da37d3709b0c7971effdfa09dadf
MHST0621 <1186217640@qq.com>
2021-04-18 12:11:50 +0800

    Daily practice

--- algorithm/listnode/isPalindrome.java
+++ algorithm/listnode/isPalindrome.java
@@ -7,6 +7,10 @@ import java.util.Stack;
  * @Date 2021/3/25 14:19
  * @Version 1.0
  */
+
+/**判断链表是否为回文结构
+ *
+ */
 public class isPalindrome {
 
     static class Node{
@@ -18,6 +22,7 @@ public class isPalindrome {
         }
     }
 
+    //时间复杂度O（n） 空间O（N）
     public boolean isPalindrome1(Node head) {
 
         Stack<Node> stack = new Stack<Node>();
@@ -38,6 +43,41 @@ public class isPalindrome {
     }
 
 
+    public boolean isPalindrome3(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Stack<Node> stack = new Stack<Node>();
+        Node right = head;
+        Node left = head;
+
+        while (right.next != null && right.next.next != null) {
+            right = right.next.next;
+            left = left.next;
+        }
+
+        right = left.next;
+
+        while (right != null) {
+            stack.push(right);
+            right = right.next;
+        }
+
+        while (!stack.isEmpty()) {
+            if (head.value != stack.pop().value) {
+                return false;
+            }
+            head = head.next;
+        }
+
+        return true;
+
+    }
+
+    //空间复杂度为O（1）
+    //用三个指针找到链表中间节点，并把右半区反转
+    //然后再通过遍历判断是否为回文
     public boolean isPalindrome2(Node head){
 
         if (head == null || head.next == null) {
@@ -48,31 +88,45 @@ public class isPalindrome {
         Node node2 = head;
         Node node3 = null;
 
+        //当快指针溢出时慢指针刚好处于链表中间
         while (node2.next != null && node2.next.next != null) {
             node1 = node1.next;
             node2 = node2.next.next;
         }
 
-        node2 = node1.next;
-        node1.next = null;
+        node2 = node1.next; //node2 为右半区第一个节点
+        node1.next = null; //中间节点指向空
 
         while (node2 != null) {
             node3 = node2.next;
             node2.next = node1;
-            node1 =node2;
+            node1 = node2;
             node2 = node3;
         }
 
-        node3 = node1;
+        node3 = node1; // node3保存最后一个节点
         node2 = head;
-
+        boolean res = true;
         while (node1 != null && node2 != null) {
-            if (node1.value == node2.value) {
-                node1 = node1.next;
-                node2 = node2.next;
-            }else return false;
+            if (node1.value != node2.value) {
+                res = false; //无论怎么样都要恢复链表
+                break;
+            }
+            node1 = node1.next;
+            node2 = node2.next;
         }
-        return true;
+
+        node1 = node3.next;
+        node3.next = null;
+
+        while (node1 != null) {
+            node2 = node1.next;
+            node1.next = node3;
+            node3 = node1;
+            node1 = node2;
+        }
+
+        return res;
     }
 
 
--- algorithm/listnode/mergeTwoList.java
+++ algorithm/listnode/mergeTwoList.java
@@ -17,34 +17,34 @@ public class mergeTwoList {
     }
 
 
-
-
     public Node mergetwolist1(Node l1, Node l2) {
 
         if (l1 == null || l2 == null) {
             return (l1 == null)? l1:l2;
         }
 
-        Node head = (l1.value <= l2.value)? l1:l2;
-        Node n1 = (head == l1)? l1:l2;
-        Node n2 = (head == l1)? l2:l1;
-        Node pre = null;
-        Node next = null;
+        Node head = null;
 
-        while (n1 != null && n2 != null) {
-            if (n1.value <= n2.value) {
-                pre = n1;
-                n1 = n1.next;
-            } else {
-                next = n2.next;
-                pre.next = n2;
-                n2.next = n1;
-                pre = n2;
-                n2 = next;
+        head = (l1.value < l2.value) ? l1:l2; //使链表开头值小的链表作为合并后的链表保存在head
+
+        Node cur1 = head == l1 ? l1 : l2; //默认cur1能作为head开头的链表
+        Node cur2 = head == l1 ? l2 : l1;
+        Node pre = null; //用来保存上一次最小值的节点
+        Node next = null; // 插入时的中间节点
+        while (cur1 != null && cur2 != null) {
+            if (cur1.value < cur2.value) { //如果cur1小于cur2的值则不变，cur1继续向下遍历
+                pre = cur1;
+                cur1 = cur1.next;
+            } else { //如果cur1大于cur2，则cur2插入到pre.next cur2.next指向cur1 并把cur2 保存在pre中
+                next = cur2.next;
+                pre.next = cur2;
+                cur2.next = cur1;
+                pre = cur2;
+                cur2 = next;
             }
         }
 
-        pre.next = n1 == null ? n2:n1;
+        pre.next = cur1 == null ? cur2 : cur1;
         return head;
 
     }
@@ -64,8 +64,9 @@ public class mergeTwoList {
         node4.next = node5;
         node5.next = null;
 
-        Node result = mergetwolist.mergetwolist1(node1,node4);
-        while(result != null) {
+        Node result =  mergetwolist.mergetwolist1(node1,node4);
+
+        while (result != null) {
             System.out.println(result.value);
             result = result.next;
         }
