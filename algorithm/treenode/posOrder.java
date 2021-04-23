80718461b7519df5fbc45b5ef9b2e56ad52ff25f
MHST0621 <1186217640@qq.com>
2021-04-23 15:29:22 +0800

    QQ

--- algorithm/treenode/posOrder.java
+++ algorithm/treenode/posOrder.java
@@ -1,5 +1,7 @@
 package algorithm.treenode;
 
+import java.util.Stack;
+
 /**
  * @Author Mahe
  * @Date 2021/4/20 10:14
@@ -7,13 +9,33 @@ package algorithm.treenode;
  */
 	//后续遍历
 public class posOrder {
-	public void posOrderRecur(Node head) {
+	public void posOrderRecur01(Node head) {
 		if (head == null) {
 			return;
 		}
 
-		posOrderRecur(head.left);
-		posOrderRecur(head.right);
+		posOrderRecur01(head.left);
+		posOrderRecur01(head.right);
 		System.out.println(head.value);
+	}
+
+	public void posOrderRecur02(Node head) {
+		if (head == null) {
+			return;
+		}
+		Stack<Node> s1 = new Stack<Node>();
+		Stack<Node> s2 = new Stack<Node>();
+		Node cur = null;
+		s1.push(head);
+		while (!s1.isEmpty()) {
+			cur = s1.pop();
+			s2.push(cur);
+			if (cur.left != null) s1.push(cur.left);
+			if (cur.right != null) s1.push(cur.right);
+		}
+
+		while (!s2.isEmpty()) {
+			System.out.println(s2.pop().value);
+		}
 	}
 }
